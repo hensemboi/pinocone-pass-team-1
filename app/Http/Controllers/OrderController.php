b82c7ffe4ab26b22d64cbe0a14cb110027d39840
hensemboi <77122187+hensemboi@users.noreply.github.com>
2022-05-15 20:36:34 +0700

    Delete merge bug

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,12 +8,6 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
     public function fetchAll(){
-<<<<<<< HEAD
-        return Order::All();
-    }
-=======
         return Order::all();
     }
-
->>>>>>> origin/pinocone-requestDashboard
 }
--- resources/js/components/hooks/search.js
+++ resources/js/components/hooks/search.js
@@ -1,30 +1,4 @@
 import { ref, computed, watch } from "vue";
-
-<<<<<<< HEAD
-export default function useSearch(module = 'menu/fetchAllCategories') {
-  async function getAllCategories (){
-    if (!store.getters["menu/getIsCategoriesPopulated"]) {
-        try {
-            return await store.dispatch(module);
-        } catch (error) {
-            console.log(
-                error.errorMessage || "Failed to fetch categories"
-            );
-        }
-    } else {
-        console.log("The fetching is not executing!");
-        computed(() =>store.getters['menu/getAllCategories']);
-    }
-    return true
-}
-
-  return {
-      enteredSearchTerm,
-    availableItems,
-    updateSearch,
-  };
-} 
-=======
 export default function useSearch(items, searchProp) {
     const enteredSearchTerm = ref("");
     const activeSearchTerm = ref("");
@@ -61,4 +35,3 @@ export default function useSearch(items, searchProp) {
         updateSearch,
     };
 }
->>>>>>> origin/pinocone-requestDashboard
